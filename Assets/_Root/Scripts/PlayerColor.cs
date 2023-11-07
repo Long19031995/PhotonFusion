5e77c783d9b5f0a8e88f19135e03d26812809b1d
Kim Long <=>
2023-11-07 18:00:22 +0700

    Photon Fusion 104 Shared

---
+++ Assets/_Root/Scripts/PlayerColor.cs
@@ -0,0 +1,24 @@
+using Fusion;
+using UnityEngine;
+
+public class PlayerColor : NetworkBehaviour
+{
+    public MeshRenderer MeshRenderer;
+
+    [Networked(OnChanged = nameof(NetworkColorChanged))]
+    public Color NetworkedColor { get; set; }
+
+    private static void NetworkColorChanged(Changed<PlayerColor> changed)
+    {
+        changed.Behaviour.MeshRenderer.material.color = changed.Behaviour.NetworkedColor;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Changing the material color here directly does not work since this code is only executed on the client pressing the button and not on every client.
+            NetworkedColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+        }
+    }
+}
